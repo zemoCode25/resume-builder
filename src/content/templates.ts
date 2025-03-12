7034054e228bafe18845c84b9fd329441eec1d08
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-03-12 15:31:57 +0800

    Created template array,  mapped the array to display UI

---
+++ src/content/templates.ts
@@ -0,0 +1,23 @@
+type Template = {
+  src: string;
+  alt: string;
+};
+
+export const templates: Template[] = [
+  {
+    src: "../../public/assets/harvard-resume-free-google-docs-template-t.jpg",
+    alt: "Harvard standard template 1",
+  },
+  {
+    src: "../../public/assets/Stanford-Resume-Template-Example.svg",
+    alt: "Harvard standard template 2",
+  },
+  {
+    src: "../../public/assets/Harvard-Resume-Template-Example.svg",
+    alt: "Harvard standard template 3",
+  },
+  {
+    src: ".../../public/assets/88883612.png",
+    alt: "Harvard standard template 4",
+  },
+];
--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -1,6 +1,9 @@
 import { Overlay } from "../components/overlay/Overlay";
+import { templates } from "../content/templates";
+import { useState } from "react";
 
 export function Templates() {
+  // const [selectedTemplate, setSelectedTemplate] = useState("");
   return (
     <main className="min-h-screen flex flex-col justify-center items-center bg-stone-100 py-5">
       <div className="flex justify-center flex-col items-center mb-5">
@@ -10,34 +13,15 @@ export function Templates() {
         <p className=" text-center">Select a Harvard style resume template</p>
       </div>
       <div className="gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:lg:grid-cols-4 px-10">
-        <div className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200">
-          <img
-            className="w-full h-full object-cover"
-            src="../../public/assets/harvard-resume-free-google-docs-template-t.jpg"
-            alt=""
-          />
-        </div>
-        <div className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200">
-          <img
-            className="w-full h-full object-cover"
-            src="../../public/assets/Stanford-Resume-Template-Example.svg"
-            alt=""
-          />
-        </div>
-        <div className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200">
-          <img
-            className="w-full h-full object-cover"
-            src="../../public/assets/Harvard-Resume-Template-Example.svg"
-            alt=""
-          />
-        </div>
-        <div className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200">
-          <img
-            className="w-full h-full object-cover"
-            src="../../public/assets/PR10.png"
-            alt=""
-          />
-        </div>
+        {templates.map((template) => (
+          <div className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200">
+            <img
+              className="w-full h-full object-cover"
+              src={template.src}
+              alt={template.alt}
+            />
+          </div>
+        ))}
       </div>
       <Preview />
       <Overlay />
