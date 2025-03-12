17df313f61d12ef465b319bbe9555e25c14e7c0d
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-03-12 17:27:44 +0800

    Set up preview usestate

--- src/content/templates.ts
+++ src/content/templates.ts
@@ -1,22 +1,27 @@
 type Template = {
+  readonly id: number;
   src: string;
   alt: string;
 };
 
 export const templates: Template[] = [
   {
+    id: 1,
     src: "../../public/assets/harvard-resume-free-google-docs-template-t.jpg",
     alt: "Harvard standard template 1",
   },
   {
+    id: 2,
     src: "../../public/assets/Stanford-Resume-Template-Example.svg",
     alt: "Harvard standard template 2",
   },
   {
+    id: 3,
     src: "../../public/assets/Harvard-Resume-Template-Example.svg",
     alt: "Harvard standard template 3",
   },
   {
+    id: 4,
     src: ".../../public/assets/88883612.png",
     alt: "Harvard standard template 4",
   },
--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -3,7 +3,14 @@ import { templates } from "../content/templates";
 import { useState } from "react";
 
 export function Templates() {
-  // const [selectedTemplate, setSelectedTemplate] = useState("");
+  const [selectedTemplate, setSelectedTemplate] = useState(0);
+  const [isPreviewOpen, setPreview] = useState(false);
+
+  function handleTemplateClick(key: number): void {
+    setPreview((prevPreviewState) => !prevPreviewState);
+    setSelectedTemplate(key);
+  }
+
   return (
     <main className="min-h-screen flex flex-col justify-center items-center bg-stone-100 py-5">
       <div className="flex justify-center flex-col items-center mb-5">
@@ -14,7 +21,10 @@ export function Templates() {
       </div>
       <div className="gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:lg:grid-cols-4 px-10">
         {templates.map((template) => (
-          <div className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200">
+          <div
+            onClick={() => handleTemplateClick(template.id)}
+            className="p-4 rounded-sm border border-gray-300 shadow-all-md cursor-pointer hover:border-gray-500 shadow-all-lg transition-shadow duration-200"
+          >
             <img
               className="w-full h-full object-cover"
               src={template.src}
@@ -23,8 +33,13 @@ export function Templates() {
           </div>
         ))}
       </div>
-      <Preview />
-      <Overlay />
+
+      {isPreviewOpen && (
+        <div>
+          <Preview />
+          <Overlay />
+        </div>
+      )}
     </main>
   );
 }
