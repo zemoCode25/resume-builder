1a92c59c720d370d8bd40e0fbbac3742a105c75e
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-03-21 20:18:43 +0800

    Created projects component

--- src/main/resume/ResumeForm.tsx
+++ src/main/resume/ResumeForm.tsx
@@ -11,24 +11,24 @@ export function ResumeForm() {
         <button className="cursor-pointer text-2xl px-3 hover:font-bold">
           {"<"}
         </button>
-        <p className=" font-semibold">Skills</p>
+        <p className=" font-semibold">Projects</p>
         <button className="cursor-pointer text-2xl px-3 hover:font-bold">
           {">"}
         </button>
       </div>
-      <SkillList />
+      <ProjectList />
     </div>
   );
 }
 
-export function SkillList() {
+export function ProjectList() {
   const [skillCount, setSkillCount] = useState(1);
   return (
     <>
       <div className="w-3/4">
         <div className="grid grid-cols-1 items-center gap-3 w-full">
           {Array.from({ length: skillCount }).map((_, i) => (
-            <Skill key={i} />
+            <Project key={i} />
           ))}
         </div>
       </div>
@@ -38,21 +38,34 @@ export function SkillList() {
           onClick={() => setSkillCount(skillCount + 1)}
         >
           <CirclePlus />
-          Add Skill
+          Add Project
         </Button>
       </div>
     </>
   );
 }
 
-export function Skill() {
+export function Project() {
   return (
-    <form action="" className="w-full flex gap-3 bg-white p-5 rounded-md">
+    <form
+      action=""
+      className="w-full flex flex-col gap-3 bg-white p-5 rounded-md shadow-all-sm"
+    >
       <div className="w-full flex flex-col gap-1">
         <label htmlFor="" className="font-semibold text-gray-700">
-          Skill
+          Project Name
         </label>
         <Input placeholder="e.g. Time Management" />
+      </div>
+      <div className="w-full flex flex-col gap-1">
+        <label htmlFor="" className="font-semibold text-gray-700">
+          Project description
+        </label>
+        <textarea
+          name=""
+          id=""
+          className="w-full p-3 text-sm border outline-none min-h-32 rounded-sm"
+        ></textarea>
       </div>
     </form>
   );
