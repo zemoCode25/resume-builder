924a419641d54d4e88e2fcd36a15a16b92566c9f
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-03-24 13:01:18 +0800

    Refactor

--- src/main/forms/Course.tsx
+++ src/main/forms/Course.tsx
@@ -2,7 +2,7 @@ import { Input } from "@/components/ui/input";
 
 export function Course() {
   return (
-    <form action="" className="w-full flex gap-3 bg-white p-5 rounded-md">
+    <form className="w-full flex gap-3 bg-white p-5 rounded-md">
       <div className="w-full flex flex-col gap-1">
         <label htmlFor="" className="font-semibold text-gray-700">
           Course
