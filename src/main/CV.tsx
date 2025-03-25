28a85865230dade2d6c14c61ebd0c229f1b86e21
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-03-25 14:23:46 +0800

    used context in cover letter

--- src/main/CV.tsx
+++ src/main/CV.tsx
@@ -1,7 +1,14 @@
 import { CVView } from "./cover-letter/CVView";
 import { CVForm } from "./cover-letter/CVForm";
+import { useContext } from "react";
+import { AppTypeContext } from "@/contexts/AppContext";
 
 export function CV() {
+  const { appType } = useContext(AppTypeContext);
+
+  if (appType !== "cover-letter") {
+    return;
+  }
   return (
     <div>
       <CVView />
