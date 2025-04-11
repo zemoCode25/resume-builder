04d1633620e1fbe7078e53a173708cb7906310b5
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-04-11 19:16:48 +0800

    Added Company details in cover letter component

--- src/main/cover-letter/CVForm.tsx
+++ src/main/cover-letter/CVForm.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 export function CVForm() {
   const { appType, setAppType } = useContext(AppTypeContext);
   return (
-    <div className="w-1/2 flex flex-col justify-center items-center gap-3">
+    <div className="w-1/2 flex flex-col justify-center items-center gap-4">
       <Tabs defaultValue={appType} className="w-3/4" onValueChange={setAppType}>
         <TabsList className="w-full">
           <TabsTrigger className="cursor-pointer" value="resume">
@@ -18,6 +18,7 @@ export function CVForm() {
         </TabsList>
       </Tabs>
       <Personal />
+      <Company />
     </div>
   );
 }
@@ -25,18 +26,48 @@ export function CVForm() {
 export function Personal() {
   return (
     <div className="w-3/4 flex flex-col bg-white rounded-sm p-5 shadow-all-sm">
-      <h2 className="mb-2 text-xl font-bold text-gray-700">Personal Details</h2>
+      <h2 className="mb-2 text-lg font-bold text-gray-700">Personal Details</h2>
       <form action="">
         <div className="grid grid-cols-2 gap-10">
           <div className="w-full flex flex-col gap-1">
-            <label className="font-semibold text-gray-700" htmlFor="">
+            <label className="text-gray-700" htmlFor="">
               First Name
             </label>
             <Input></Input>
           </div>
           <div className="w-full flex flex-col gap-1">
-            <label className="font-semibold text-gray-700" htmlFor="">
+            <label className="text-gray-700" htmlFor="">
               Last Name
+            </label>
+            <Input></Input>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export function Company() {
+  return (
+    <div className="w-3/4 flex flex-col bg-white rounded-sm p-5 shadow-all-sm">
+      <h2 className="mb-2 text-lg font-bold text-gray-700">Company Details</h2>
+      <form action="">
+        <div className="grid grid-cols-2 gap-10">
+          <div className="w-full flex flex-col gap-1">
+            <label className="text-gray-700" htmlFor="">
+              First Name
+            </label>
+            <Input></Input>
+          </div>
+          <div className="w-full flex flex-col gap-1">
+            <label className="text-gray-700" htmlFor="">
+              Last Name
+            </label>
+            <Input></Input>
+          </div>
+          <div className="w-full flex flex-col gap-1">
+            <label className="text-gray-700" htmlFor="">
+              Title
             </label>
             <Input></Input>
           </div>
