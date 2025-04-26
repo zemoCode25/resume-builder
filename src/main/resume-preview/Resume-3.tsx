0dbd5b29110f1c9b61ec1ee97af4289c780684c9
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-04-26 23:16:33 +0800

    Added Feature: Prevent Experience component to preview w/o value

--- src/main/resume-preview/Resume-3.tsx
+++ src/main/resume-preview/Resume-3.tsx
@@ -86,17 +86,12 @@ export function ProjectPreview({
 }: {
   projectList: ProjectType[];
 }) {
-  console.log("PROJECTS: ");
-  if (projectList[0]?.projectDescription)
-    console.log(projectList[0]?.projectDescription);
-
   // Check whether there is a value present in the state
-
   for (const projectItem of projectList) {
     // Excluded id because it is a default data
     // Excluded projectDescription from projectData as it has a value by default
     const { id: _id, projectDescription, ...projectData } = projectItem;
-    let isDescriptionForund: boolean = false;
+    let isDescriptionFound: boolean = false;
 
     const isValueFound = Object.values(projectData).some(
       (projectItemData) => projectItemData
@@ -105,11 +100,11 @@ export function ProjectPreview({
     for (const projectDescriptionItem of projectDescription || []) {
       const { description } = projectDescriptionItem;
       if (description) {
-        isDescriptionForund = true;
+        isDescriptionFound = true;
       }
     }
 
-    if (!isValueFound && !isDescriptionForund) {
+    if (!isValueFound && !isDescriptionFound) {
       return;
     }
   }
@@ -148,8 +143,32 @@ export function ExperiencePreview({
 }: {
   experienceList: ExperienceType[];
 }) {
-  console.log("EDUCATION LIST: ");
-  console.log(experienceList);
+  // Check whether there is a value present in the state
+  for (const experiencetItem of experienceList) {
+    // Excluded id because it is a default data
+    // Excluded projectDescription from projectData as it has a value by default
+    const { id: _id, jobDescription, ...experienceData } = experiencetItem;
+    let isDescriptionFound: boolean = false;
+
+    const isValueFound = Object.values(experienceData).some(
+      (experienceItemData) => experienceItemData
+    );
+
+    for (const experienceDescriptionItem of jobDescription || []) {
+      const { description } = experienceDescriptionItem;
+      if (description) {
+        isDescriptionFound = true;
+      }
+    }
+
+    if (!isValueFound && !isDescriptionFound) {
+      return;
+    }
+  }
+
+  if (experienceList.length === 0) {
+    return;
+  }
   return (
     <div>
       <h2 className="text-lg font-semibold border-b border-b-gray-900 pb-0.5">
