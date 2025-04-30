e133e48a909f852c79baa0de7264da7594cd9bd3
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-04-30 11:49:23 +0800

    Seperated experience component in Tech document

--- src/main/pdf/resume/resume-document/TechDocument.tsx
+++ src/main/pdf/resume/resume-document/TechDocument.tsx
@@ -1,5 +1,5 @@
 import { Document, Page, Text, View } from "@react-pdf/renderer";
-import { DefaultForm } from "@/types/templates/form-types";
+import { DefaultForm, ExperienceType } from "@/types/templates/form-types";
 import { PersonalType } from "@/types/templates/form-types";
 import { styles } from "../style";
 import { options } from "@/utils/main-utils";
@@ -21,47 +21,7 @@ export function TechDocument({
       <Page size="A4" style={styles.page}>
         <View>
           <Personal personalData={personal} />
-          {/* Experience */}
-          <View style={styles.section}>
-            <View style={styles?.headerContainer}>
-              <Text style={styles.header}>Experience</Text>
-            </View>
-            <View>
-              {experience?.map((experienceItem) => (
-                <View style={styles.section}>
-                  <View style={styles.flexContainer}>
-                    <Text style={styles.text}>
-                      {experienceItem?.position || ""}
-                    </Text>
-                    <Text style={styles.text}>{`${
-                      experienceItem?.startDate?.toLocaleDateString(
-                        undefined,
-                        options
-                      ) || ""
-                    } - ${
-                      experienceItem?.endDate?.toLocaleDateString(
-                        undefined,
-                        options
-                      ) || ""
-                    }`}</Text>
-                  </View>
-                  <Text style={styles.text}>
-                    {experienceItem?.company || ""}
-                  </Text>
-                  <View style={styles.unorderedList}>
-                    {experienceItem?.jobDescription?.map((description) => {
-                      if (!description?.description) return;
-                      return (
-                        <Text key={description?.id} style={styles?.text}>{`• ${
-                          description?.description || ""
-                        }`}</Text>
-                      );
-                    })}
-                  </View>
-                </View>
-              ))}
-            </View>
-          </View>
+          <Experience experienceData={experience} />
           {/* Education */}
           <View style={styles.section}>
             <View style={styles?.headerContainer}>
@@ -164,6 +124,51 @@ export function Personal({ personalData }: { personalData: PersonalType }) {
       <Text style={styles.text}>{phoneNumber}</Text>
       <Text style={styles.text}>{email}</Text>
       <Text style={styles.text}>{`${city || ""}, ${country || ""}`}</Text>
+    </View>
+  );
+}
+
+export function Experience({
+  experienceData,
+}: {
+  experienceData: ExperienceType[];
+}) {
+  return (
+    <View style={styles.section}>
+      <View style={styles?.headerContainer}>
+        <Text style={styles.header}>Experience</Text>
+      </View>
+      <View>
+        {experienceData?.map((experienceItem) => (
+          <View style={styles.section}>
+            <View style={styles.flexContainer}>
+              <Text style={styles.text}>{experienceItem?.position || ""}</Text>
+              <Text style={styles.text}>{`${
+                experienceItem?.startDate?.toLocaleDateString(
+                  undefined,
+                  options
+                ) || ""
+              } - ${
+                experienceItem?.endDate?.toLocaleDateString(
+                  undefined,
+                  options
+                ) || ""
+              }`}</Text>
+            </View>
+            <Text style={styles.text}>{experienceItem?.company || ""}</Text>
+            <View style={styles.unorderedList}>
+              {experienceItem?.jobDescription?.map((description) => {
+                if (!description?.description) return;
+                return (
+                  <Text key={description?.id} style={styles?.text}>{`• ${
+                    description?.description || ""
+                  }`}</Text>
+                );
+              })}
+            </View>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
